1060de6583063dd18661f7e78323f87b1d939f4b
i1 <i1@i1-uBOOK-E756>
2017-04-13 15:32:27 +0200

    counter++

--- src/main/java/com/michaelrice/demo/DemoActivator.java
+++ src/main/java/com/michaelrice/demo/DemoActivator.java
@@ -4,16 +4,16 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class DemoActivator implements BundleActivator {
-
+	private static int counter =0;  
 	
     @Override
     public void start(BundleContext bundleContext) throws Exception {
-        System.out.println("STARTING DEMO 1.0.5+1..: hello, world @[" +bundleContext+"]");
+        System.out.println("STARTING DEMO 1.0.5+1..: hello#"+(counter++)+", world @[" +bundleContext+"]");
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
-        System.out.println("STOPPING DEMO");
+        System.out.println("STOPPING DEMO +++" + bundleContext +"+++");
     }
 
 }
