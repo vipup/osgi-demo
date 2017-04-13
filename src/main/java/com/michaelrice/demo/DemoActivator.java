e19d78d62b0c4cf7655a1a0933b6b8c3775aa2b3
i1 <i1@i1-uBOOK-E756>
2017-04-13 14:35:17 +0200

    1.0.5-S,....

--- src/main/java/com/michaelrice/demo/DemoActivator.java
+++ src/main/java/com/michaelrice/demo/DemoActivator.java
@@ -8,7 +8,7 @@ public class DemoActivator implements BundleActivator {
 	
     @Override
     public void start(BundleContext bundleContext) throws Exception {
-        System.out.println("STARTING DEMO 1.0.4+1........: hello, world @[" +bundleContext+"]");
+        System.out.println("STARTING DEMO 1.0.5..: hello, world @[" +bundleContext+"]");
     }
 
     @Override
